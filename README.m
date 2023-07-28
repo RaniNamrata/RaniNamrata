60b8675eb0e687f1315b96a89a70b7a0c1345b00
RaniNamrata <140816433+RaniNamrata@users.noreply.github.com>
2023-07-28 18:01:27 +0530

    Namrata Rani
    Student at DSEU Kasturba pitampura campus.
    Intern@codsoft

---
+++ README.m
@@ -0,0 +1,11 @@
+- 👋 H I’m @RaniNamrata
+- 👀 I’m interested in Web development and want to be a front end developer.
+- 🌱 I’m currently learning different coding languages..
+- 💞️ I’m looking to collaborate on Microsoft 
+- 📫 How to reach me https://www.linkedin.com/in/namrata-rani-24b847277
+- ...
+
+<!---
+RaniNamrata/RaniNamrata is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
